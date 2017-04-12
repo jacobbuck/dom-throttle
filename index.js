b4e7ec0910fd28f264b090887c33f52e03cf870d
Jacob Buck <buck.jacob@gmail.com>
2017-04-12 19:16:48 +1200

    Code style changes

--- index.js
+++ index.js
@@ -8,26 +8,26 @@
  * @param func {Function} The function to throttle.
  * @return {Function} Returns the new throttled function.
  */
-module.exports = function domThrottle (func) {
-	var requestId = false;
+module.exports = function domThrottle(func) {
+  var requestId = false;
 
-	var throttled = function () {
-		var context = this;
-		var args = arguments;
-		if (requestId === false) {
-			requestId = window.requestAnimationFrame(function () {
-				func.apply(context, args);
-				requestId = false;
-			});
-		}
-	};
+  var throttled = function() {
+    var context = this;
+    var args = arguments;
+    if (requestId === false) {
+      requestId = window.requestAnimationFrame(function() {
+        func.apply(context, args);
+        requestId = false;
+      });
+    }
+  };
 
-	throttled.cancel = function () {
-		if (requestId !== false) {
-			window.cancelAnimationFrame(requestId);
-			requestId = false;
-		}
-	};
+  throttled.cancel = function() {
+    if (requestId !== false) {
+      window.cancelAnimationFrame(requestId);
+      requestId = false;
+    }
+  };
 
-	return throttled;
+  return throttled;
 };
